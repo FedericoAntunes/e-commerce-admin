db85b095e1e86fbee153d8b86250153faebd610c
Sebasan22 <callofsebas@hotmail.com>
2023-03-26 19:25:00 -0300

    Update EditUserModal.jsx

--- src/components/partials/EditUserModal.jsx
+++ src/components/partials/EditUserModal.jsx
@@ -97,19 +97,15 @@ function EditUserModal({
                     />
                   </div>
                   <div className="col-span-6 sm:col-span-3">
-                    <label
-                      for="department"
-                      className="block mb-2 text-sm font-medium text-gray-900"
+                    <p className="block mb-2 text-sm font-medium text-gray-900">
+                      Give admin privileges
+                    </p>
+                    <button
+                      type="submit"
+                      className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                     >
-                      Department
-                    </label>
-                    <input
-                      type="text"
-                      name="department"
-                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-                      placeholder="Development"
-                      required=""
-                    />
+                      Give admin
+                    </button>
                   </div>
                   <div className="col-span-6 sm:col-span-3">
                     <label
