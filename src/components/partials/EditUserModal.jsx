a8cee06847a303c4b5704166180b07e5951964c5
Sebasan22 <callofsebas@hotmail.com>
2023-03-27 09:34:16 -0300

    Update EditUserModal.jsx

--- src/components/partials/EditUserModal.jsx
+++ src/components/partials/EditUserModal.jsx
@@ -97,28 +97,17 @@ function EditUserModal({
                     />
                   </div>
                   <div className="col-span-6 sm:col-span-3">
-                    <p className="block mb-2 text-sm font-medium text-gray-900">
-                      Give admin privileges
-                    </p>
-                    <button
-                      type="submit"
-                      className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                    >
-                      Give admin
-                    </button>
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for="company"
+                      for="user-image"
                       className="block mb-2 text-sm font-medium text-gray-900"
                     >
-                      Company
+                      Avatar
                     </label>
                     <input
-                      type="number"
-                      name="company"
+                      type="text"
+                      name="user-image"
                       className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-                      placeholder="123456"
+                      placeholder={`${user.avatar}`}
                       required=""
                     />
                   </div>
