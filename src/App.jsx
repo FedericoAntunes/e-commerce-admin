59a09b931b0a2f1f6bd72c0a065a50e4b486eaa8
Sebasan22 <callofsebas@hotmail.com>
2023-03-27 19:38:05 -0300

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import AdminPanel from "./components/AdminPanel";
+import UserPanel from "./components/UserPanel";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<AdminPanel />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<UserPanel />} />
       </Routes>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import SideBar from "./partials/SideBar";
+export default function Home() {
+  return (
+    <>
+      <SideBar />
+      <div>Estas en home de Admin</div>
+    </>
+  );
+}
