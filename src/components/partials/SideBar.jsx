c7f54b2da137e521f0f7824f5956a8eb79699dc7
Sebasan22 <callofsebas@hotmail.com>
2023-03-29 15:40:32 -0300

    Update SideBar.jsx

--- src/components/partials/SideBar.jsx
+++ src/components/partials/SideBar.jsx
@@ -95,7 +95,7 @@ export default function SideBar() {
                     <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
                     <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
+                  <span className="flex-1 ml-12 whitespace-nowrap">Inbox</span>
                   <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full">
                     3
                   </span>
@@ -184,7 +184,9 @@ export default function SideBar() {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Free Slot
+                  </span>
                 </Link>
               </li>
             </ul>
