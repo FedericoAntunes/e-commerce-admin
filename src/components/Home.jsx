f3361d2075ed8038349281998ccb68c116dcc7f4
Sebasan22 <callofsebas@hotmail.com>
2023-04-12 20:00:38 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,7 @@ export default function Home() {
       <Header />
       {products ? (
         <div className="flex mt-16 bg-gray-100 mx-auto flex-wrap justify-center w-full overflow-x-auto">
-          <div className="md:contents columns-1 hidden">
+          <div className="columns-1 hidden md:inline">
             <GrandielChart />
             <BarChart />
           </div>
