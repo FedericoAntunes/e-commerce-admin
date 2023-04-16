ac50ea3595ef2c9021a1f06ec7068cb910e61df8
sebasan22 <callofsebas@hotmail.com>
2023-04-15 22:50:03 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -63,32 +63,19 @@ function Login() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
               Sign in to your admin account
             </h1>
-            <div className="flex justify-end text-sm font-medium">
-              <Link
-                to="https://no-hunger-food.vercel.app"
-                className="text-green-500 hover:text-green-700 focus:text-green-700 hover:underline transform hover:scale-110 focus:scale-110 transition-all"
-              >
-                <FontAwesomeIcon
-                  icon={faUtensils}
-                  className="text-green-500 mr-2"
-                />
-                No Hunger
-              </Link>
-            </div>
-
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-black"
+                  className="block mb-2 text-lg font-medium text-black tracking-wide"
                 >
-                  Your email
+                  Admin Email
                 </label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5"
                   placeholder="Insert your email..."
                   value={inputEmail}
                   onChange={(event) => setInputEmail(event.target.value)}
@@ -97,7 +84,7 @@ function Login() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-black"
+                  className="block mb-2 text-lg font-medium text-black tracking-wide"
                 >
                   Password
                 </label>
@@ -106,7 +93,7 @@ function Login() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5"
                   value={inputPassword}
                   onChange={(event) => setInputPassword(event.target.value)}
                 />
@@ -118,7 +105,7 @@ function Login() {
                       id="remember"
                       aria-describedby="remember"
                       type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                      className="w-4 h-4  border-gray-300 rounded cursor-pointer text-green-500 bg-gray-50 focus:ring-0 focus:ring-offset-0 active:ring-0"
                       required=""
                     />
                   </div>
@@ -131,19 +118,9 @@ function Login() {
                     </label>
                   </div>
                 </div>
-                <div className="flex justify-center text-sm font-medium">
-                  <button
-                    className="m-2 md:px-4 md:py-2 rounded-md text-white bg-green-500 md:hover:bg-green-600 focus:bg-green-600 transition-colors"
-                    type="button"
-                    onClick={handleFillInputs}
-                  >
-                    <FontAwesomeIcon icon={faPencilAlt} className="mr-2" />
-                    Fill Inputs
-                  </button>
-                </div>
                 <Link
                   to="#"
-                  className="text-base font-medium text-green-600 md:hover:underline"
+                  className="text-base font-medium text-green-600 lg:hover:underline"
                 >
                   Forgot password?
                 </Link>
@@ -161,11 +138,46 @@ function Login() {
   </div>*/}
               <button
                 type="submit"
-                className="w-full text-gray-200	bg-yellow-500 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                className="w-full text-gray-200	bg-yellow-500 lg:hover:bg-yellow-400 ease-in-out duration-300 active:bg-yellow-600 font-medium rounded-lg text-base px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
             </form>
+          </div>
+        </div>
+        <div className="bg-white text-left border shadow-lg rounded-lg mt-10 w-full sm:w-[600px] px-5 py-8 lg:absolute lg:right-0 lg:w-[200px] lg:rounded-r-none mr-0">
+          <h3 className="mb-2 font-semibold">Fill fields with test data</h3>
+          <p className="text-sm">
+            To simplify the access to the application, we provide the
+            information of an existing account to fill the fields:
+          </p>
+          <div className="flex justify-center text-sm font-medium border-b pb-2">
+            <button
+              className="m-2 p-2 md:px-4 md:py-2  rounded-md text-white bg-green-500 lg:hover:bg-green-600 active:bg-green-600 transition-colors"
+              type="button"
+              onClick={handleFillInputs}
+            >
+              <div className="flex mx-auto items-center">
+                <FontAwesomeIcon icon={faPencilAlt} className="mr-2 " />
+                <p className="lg:text-left lg:w-[65px]">
+                  Insert test admin data
+                </p>
+              </div>
+            </button>
+          </div>
+          <h3 className="mb-2 font-semibold mt-6">Go to No-Hunger</h3>
+          <p className="text-sm">Go to the login of No-Hunger and enjoy!</p>
+          <div className="flex justify-center mt-2 text-sm font-medium">
+            <Link
+              to="https://no-hunger-food.vercel.app/login"
+              className="text-green-500 hover:text-green-700 focus:text-green-700 hover:underline transform hover:scale-110 focus:scale-110 transition-all"
+            >
+              <FontAwesomeIcon
+                icon={faUtensils}
+                className="text-green-500 mr-2"
+              />
+              No Hunger
+            </Link>
           </div>
         </div>
       </div>
